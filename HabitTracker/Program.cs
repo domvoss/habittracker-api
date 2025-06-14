000ea0b04e11237264f5fd316cab62064edbf0e3
domvoss <37069756+domvoss@users.noreply.github.com>
2025-06-14 21:22:50 +0100

    Removed left over weather forecast parts from setup

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,7 +1,7 @@
 using HabitTracker.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -11,13 +11,23 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddEndpointsApiExplorer();  // For minimal APIs too
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalFrontend",
+        policy => policy.WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowLocalFrontend");
 
 app.UseHttpsRedirection();
 
